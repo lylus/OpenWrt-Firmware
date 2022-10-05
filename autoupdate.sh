e620a7ba554290168842ae153cb47f47d14a7f31
Lylus <lylus@qq.com>
2022-10-05 20:28:06 +0800

    Add files via upload

---
+++ autoupdate.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# 获取固件最新版本号
+	cd /etc/config
+
+	api_url="https://api.github.com/repos/lylus/OpenWrt-Firmware/releases/latest"
+
+	new_ver=`curl ${PROXY} -s ${api_url} --connect-timeout 10| grep 'tag_name' | cut -d\" -f4`
+
+	touch ./version.txt
+	cat <<EOF > ./version.txt
+${new_ver}
+EOF
+
+	sed -i 's/v//g' ./version.txt
+	get_releases=$(cat ./version.txt)
+
+	releases_url=https://github.com/lylus/OpenWrt-Firmware/releases/download/${new_ver}/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz
+	sha256sums_url=https://github.com/lylus/OpenWrt-Firmware/releases/download/${new_ver}/sha256sums
+
+
+#	rm -rf ./version.txt
+
+
+# 检测版本和更新软件包安装sha256
+
+#	opkg update
+#	opkg install coreutils-sha256sum
+
+        old_ver=$(cat ./oldver.txt)
+#       echo "check old frp version results are: $old_ver"
+        echo "检测到本机上次更新的版本: $old_ver"
+
+#       echo "check new frp version results are: $get_releases"
+        echo "检测到最新的版本: $get_releases"
+
+if [ "$old_ver" = "$get_releases" ]
+
+then
+
+echo "没有检测到新的版本!"
+#echo "check new version"
+
+else
+# 更新固件
+
+
+	cd /tmp
+
+	wget -O sha256sums ${sha256sums_url}
+	
+	wget -O immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz ${releases_url}
+
+
+	chmod 755 immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz
+
+	sha256sum -c sha256sums 2> /dev/null | grep OK
+
+	cd /etc/config
+
+	cp -f ./version.txt ./oldver.txt
+
+	rm -rf ./version.txt
+
+	sysupgrade /tmp/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz
+
+
+
+	echo "新版本，已更新完成!"
+
+
+fi
+
+
+     echo "运行成功！"
